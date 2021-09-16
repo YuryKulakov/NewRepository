86510fc4e7c891dc4c4c065e825f647305d3efde
Yury Kulakov <sav1orxp@gmail.com>
2021-09-16 18:40:53 +0300

    add day 5

--- src/main/java/day5/Car.java
+++ src/main/java/day5/Car.java
@@ -5,31 +5,25 @@ public class Car {
     private String color;
     private int yearOfCreate;
 
-    public void setModel(String modelCar){
+    public void setModel(String modelCar) {
         model = modelCar;
-
     }
-    public String getModel(){
+    public String getModel() {
         return model;
-
     }
-    public void setColor(String colorCar){
+    public void setColor(String colorCar) {
         color = colorCar;
-
     }
-    public String getColor(){
+    public String getColor() {
         return color;
-
     }
-    public void setYearOfCreate(int yoc){
+    public void setYearOfCreate(int yoc) {
         yearOfCreate = yoc;
-
     }
-    public int getYearOfCreate(){
+    public int getYearOfCreate() {
         return yearOfCreate;
-
     }
-    void present (){
+    void present() {
         System.out.println("Вам представлен автомобиль: " + model);
         System.out.println("Цвет: " + color);
         System.out.println("Год выпуска: " + yearOfCreate);
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,7 +4,7 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
-Motorbike motorbike1 = new Motorbike("Kawasaki","black",2015);
+        Motorbike motorbike1 = new Motorbike("Kawasaki", "black", 2015);
         motorbike1.present();
     }
 }
