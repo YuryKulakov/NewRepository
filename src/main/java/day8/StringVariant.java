b9afdec12d396ce33f7800d5527faed7f1397342
Yury Kulakov <sav1orxp@gmail.com>
2021-09-26 16:36:56 +0300

    add day 8 (changes issue)

--- src/main/java/day8/StringVariant.java
+++ src/main/java/day8/StringVariant.java
@@ -3,14 +3,15 @@ package day8;
 public class StringVariant {
 
     public void Meter() {
+        String x = "";
+
         long startTime = System.currentTimeMillis();
 
-        for (int i = 0;i<=20000;i++) {
+        for (int i = 0; i <= 20000; i++) {
 
-            String x = i + " ";
-
-            System.out.print(x);
+            x += i + " ";
         }
+        System.out.print(x);
         System.out.println();
 
         long stopTime = System.currentTimeMillis();
